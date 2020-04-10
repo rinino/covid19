cabd49633ee910a0f878aac9c9fe81bec81794f9
Adriano Abbruzzese <adriano.abbruzzese@gmail.com>
2020-04-10 23:06:28 +0200

    non mi ricordo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class AppConfig {
 
     public static get APP_NAME(): string { return 'Codiv-19'; }
-    public static get GITLAB_ENDPOINT(): string { return 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/' }
-    public static get ANDAMENTO_NAZIONALE(): string {return 'dati-json/dpc-covid19-ita-andamento-nazionale.json';}
-
+    public static get GITLAB_ENDPOINT(): string { return 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/'; }
+    public static get ANDAMENTO_NAZIONALE(): string {return 'dpc-covid19-ita-andamento-nazionale.json'; }
+    public static get ANDAMENTO_NAZIONALE_LATEST(): string {return 'dpc-covid19-ita-andamento-nazionale-latest.json'; }
 }
--- src/app/services/recupero-json.service.ts
+++ src/app/services/recupero-json.service.ts
@@ -11,13 +11,49 @@ import { AndamentoNazionaleDto } from '../models/andamento-nazionale-dto';
 @Injectable()
 export class RecuperoJsonService {
   private baseUrlAndamentoNazionale: string;
+  private baseUrlAndamentoNazionleLatest: string;
 
   constructor(private httpClient: HttpClient) {
     this.baseUrlAndamentoNazionale = AppConfig.GITLAB_ENDPOINT + AppConfig.ANDAMENTO_NAZIONALE;
+    this.baseUrlAndamentoNazionleLatest = AppConfig.GITLAB_ENDPOINT + AppConfig.ANDAMENTO_NAZIONALE_LATEST;
+
   }
 
   getAndamentoNazionale(): Observable<AndamentoNazionaleDto[]> {
    return this.httpClient.get<AndamentoNazionaleDto[]>(this.baseUrlAndamentoNazionale);
   }
+
+  getDatiAndamentoNazionaleLatest(): Observable<any> {
+    return this.httpClient.get<any>(this.baseUrlAndamentoNazionleLatest);
+  }
+
+  getDataAggiornamento(): string {
+    const andamentoNazionaleLatest = this.getDatiAndamentoNazionaleLatest();
+    console.log(andamentoNazionaleLatest);
+    return 'ciao';
+  }
+
+  /**
+   * function getDatiAndamentoNazionaleLatest() {
+  var jsonResultAndamentoNazionaleLatest;
+  $.ajax({
+    dataType: "json",
+    url: urlAndamentoNazionaleLatest
+    ,
+    async: false,
+    data: jsonResultAndamentoNazionaleLatest
+    ,
+    success: function (data) {
+      jsonResultAndamentoNazionaleLatest
+        = data;
+    },
+    error: function (e) {
+      console.log("errore: " + e);
+    }
+  });
+  return jsonResultAndamentoNazionaleLatest;
+
+}
+   */
 
 }
